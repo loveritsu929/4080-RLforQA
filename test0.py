dc127481b2f1495b3f4c3aec8fc7a298b223b046
loveritsu929 <loveritsu929@gmail.com>
2019-01-09 17:48:39 -0500

    test0

---
+++ test0.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  9 15:28:52 2019
+
+@author: cxing95
+"""
+
+from utility import *
+
+#fp0 = load_file('./data/word_vocabulary','obj')
+#fp1 = load_file('./data/word_embedding','obj')
+#fp2 = load_file('./data/train_composite','obj')
+
+# training set
+#dict_keys(['fact_handles', 'sentence_source_array', 'sentence_length_array', 'sentence_symbols_array', 
+#           'sentence_numbers_array', 'fact_labels', 'answer_class', 'answer_spans'])
