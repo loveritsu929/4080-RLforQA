dfdb035e9ae0e2f5cc4b111e746b728e1fd937ee
loveritsu929 <loveritsu929@gmail.com>
2019-01-10 17:52:34 -0500

    move to ssd

--- preprocess.py
+++ preprocess.py
@@ -15,16 +15,22 @@ for record in load_file(train_dataset_path, "jsn") + load_file(develop_dataset_p
     for token in spacy_nlp(record["question"]):
         word_count[token.text] = word_count[token.text] + 1 if token.text in word_count else 1
 
+print('done word counting.')
+
 for item in load_file(glove_archive_path, "txt"):
     glove_elements = item.strip().split(" ")
 
     if glove_elements[0] in word_count:
         word_vector[glove_elements[0]] = [float(element) for element in glove_elements[1:glove_size + 1]]
 
+print('done word vector.')
+
 for word in sorted(word_count, key=word_count.get, reverse=True):
     if word in word_vector:
         word_vocabulary.append(word)
         word_embedding.append(word_vector[word])
+
+print('done word embedding.')
 
 train_composite = convert_dataset(load_file(train_dataset_path, "jsn"), word_vocabulary, True)
 develop_composite = convert_dataset(load_file(develop_dataset_path, "jsn"), word_vocabulary, False)
