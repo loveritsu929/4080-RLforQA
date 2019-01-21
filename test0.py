e19961012806d136e71486cc7e2dfa65900611c7
loveritsu929 <loveritsu929@gmail.com>
2019-01-21 18:10:40 -0500

    trainEg

--- test0.py
+++ test0.py
@@ -14,12 +14,19 @@ import json
 #fp1 = load_file('./data/word_embedding','obj')
 #fp2 = load_file('/media/data1/hotpot/train_composite','obj')   # change data path
 
-# training set
+hpqa = load_file('/media/data1/hotpot/hotpot_train_v1.1.json', 'jsn')
+
+#training set
 #dict_keys(['fact_handles', 'sentence_source_array', 'sentence_length_array', 'sentence_symbols_array', 
 #           'sentence_numbers_array', 'fact_labels', 'answer_class', 'answer_spans'])
+
 '''
 with open('./data/word_embedding', mode="rb") as file_stream:
     fp = pickle.load(file_stream)
 '''
-with open('./traineg.json', mode = 'w') as js:
-    json.dump(fp2[0], js, indent = 4)
+eg_num = 2
+with open('./trainExamples/trainEg' + str(eg_num) +'.json', mode = 'w') as js:
+    json.dump(hpqa[eg_num], js, indent = 4)
+
+
+
