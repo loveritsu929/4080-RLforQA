702405f1d718c55fecd11fd9470704a3355374c0
loveritsu929 <loveritsu929@gmail.com>
2019-01-25 20:23:16 -0500

    training

--- trainExp.py
+++ trainExp.py
@@ -16,7 +16,7 @@ from bert_serving.client import BertClient
 device = torch.device('cuda:0')
 
 batchSize = 256
-lnR = 1e-3
+lnR = 1e-2
 maxEpoches = 20
 
 modelSaveDir = './exps'
@@ -36,7 +36,9 @@ best_model_wts = copy.deepcopy(model.state_dict())
 print("Ready for trainning")
 for ep in range(maxEpoches):
     print('Start Epoch {}'.format(ep))
-    t_0 = time.time()
+    #t_0 = time.time()
+    running_loss = 0.0
+    running_corrects = 0
     for i, (q, sent, label) in enumerate(trainLoader, 0):
         q_t = torch.as_tensor(bert.encode(list(q)))
         sent_t = torch.as_tensor(bert.encode(list(sent)))
@@ -44,9 +46,6 @@ for ep in range(maxEpoches):
         sample = sample.to(device)
         
         label = label.to(device)
-        
-        running_loss = 0.0
-        running_corrects = 0
         
         out = model(sample)
         loss = lossFunction(out, label)
@@ -60,8 +59,9 @@ for ep in range(maxEpoches):
         _, preds = torch.max(out,1)  # The second return value is the index location of each maximum value found (argmax).
         running_loss += loss.item() * sample.size(0)
         running_corrects += torch.sum(preds == label.data)
-         
-        if i % 10000 == 0:
+        
+        # batchSize samples in each iteration 
+        if i % 200 == 0:
             print('Epoch {} Iteration {}: running_corrects: {} running loss = {}'.format(ep,i,running_corrects,running_loss))
              
     epoch_loss = running_loss / len(trainDataset)
@@ -72,7 +72,7 @@ for ep in range(maxEpoches):
     if epoch_acc > best_acc:
         best_acc = epoch_acc
         best_model_wts = copy.deepcopy(model.state_dict())
-        torch.save(model.state_dict(), os.path.join(modelSaveDir, 'best_model.mdl'))
+        torch.save(model.state_dict(), os.path.join(modelSaveDir, 'exp1_best_model.mdl'))
         
 print('Finish Training, Best val Acc: {:4f}'.format(best_acc))
     
