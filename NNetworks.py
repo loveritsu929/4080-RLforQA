1b36f9c794c8d0d508c00d5ff26deae53b658530
loveritsu929 <loveritsu929@gmail.com>
2019-02-19 18:49:49 -0500

    add lstm

--- NNetworks.py
+++ NNetworks.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import torch
 import torch.nn as nn
+import numpy as np
 
 class SimpleNet(nn.Module):
     def __init__(self):
@@ -20,4 +21,29 @@ class SimpleNet(nn.Module):
         
         return out
     
-    
+class MyLSTM(nn.Module):
+    def __init__(self, input_size = 1024, hidden_size = 1024, num_layers = 1):
+        super(MyLSTM, self).__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, dropout = 0.0, batch_first=False) 
+        # (seq_len, batch, hidden_size * num_directions)
+        
+        #initial weights
+        #all_weights first dimension == all hidden layers
+        nn.init.kaiming_normal_(self.lstm.all_weights[0][0], nonlinearity='leaky_relu') #weights
+        nn.init.kaiming_normal_(self.lstm.all_weights[0][1], nonlinearity='leaky_relu') #bias
+        
+        self.fc = nn.Linear(hidden_size, 2)
+        nn.init.kaiming_normal_(self.fc.weight, nonlinearity='leaky_relu')
+        
+        self.leaky_relu = nn.LeakyReLU()
+        self.softmax = nn.Softmax()
+        
+    def forward(self, x):
+
+        # Forward propagate RNN
+        out, _ = self.lstm(x)
+        out = self.leaky_relu(x)
+        # Decode hidden state of last time step
+        out = self.fc(out[-1, :, :]) #  (seq_len, batch, hidden_size * num_directions)
+        out = self.softmax(out)
+        return out
--- dlDataset.py
+++ dlDataset.py
@@ -7,6 +7,13 @@ from bert_serving.client import BertClient
 train_file = '/media/data1/hotpot/hotpot_train_v1.1.json'
 dev_file = '/media/data1/hotpot/hotpot_dev_fullwiki_v1.json'
 
+# a simple custom collate function, just to show the idea
+def my_collate(batch):
+    data = [item[0] for item in batch]
+    target = [item[1] for item in batch]
+    target = torch.LongTensor(target)
+    return [data, target]
+
 def load_file(file_path, file_type):
     if file_type == "txt":
         with open(file=file_path, mode="rt", encoding="utf-8") as file_stream:
@@ -40,7 +47,7 @@ def dump_data(data_buffer, file_path, file_type):
     else:
         pass
     
-class sentDataset(data.Dataset):
+class SentDataset(data.Dataset):
     def __init__(self, mode = 'train'):
         self.mode = mode
         #self.bertClient = BertClient(check_length=False) # localhost, max_seq_lehgth = inf
@@ -90,7 +97,7 @@ class sentDataset(data.Dataset):
     
     
 #TODO: sentence padding
-class paraDataset(data.Dataset):
+class ParaDataset(data.Dataset):
     ''' a dataset for hotpotQA
         pure DL approach
         
@@ -99,62 +106,101 @@ class paraDataset(data.Dataset):
     
     def __init__(self, mode='train'):
         self.mode = mode
-        self.bertClient = BertClient(check_length=False) # localhost, max_seq_lehgth = inf
+        #self.bertClient = BertClient(check_length=False) # localhost, max_seq_lehgth = inf
         self.fp = load_file(train_file, 'jsn') if mode == 'train' else load_file(dev_file, 'jsn') # a list of qusetions
+        self.dataset_len, self.dataset = self.make_dataset()
         
+    def make_dataset(self):
+        # need: question, para_title+sentence, label
+        # tensor n * 2048 + label
+        ds_len = 0
+        ds = []
+        for qdict in self.fp:
+            supports = qdict['supporting_facts'] # a list of 2-element lists of facts with form [title, sent_id]
+            fact_titles = [fact[0] for fact in supports] # only the titles of supporting paragraphs
+            
+            question = qdict['question']
+            context = qdict['context'] # a list of 2-element list [title, paragraph]
+            #answer = qdict['answer'] # do not really need it?
+            #qid = qdict['_id']
+            
+            # some articles in the fullwiki dev/test sets have zero paragraphs
+            if len(context) == 0:
+                context = [['some random title', ['some random stuff']]]
+            
+            for title, paragraph in context:
+                ds_len += 1
+                label = 1 if title in fact_titles else 0
+                
+                ds.append((question, [title] + paragraph, label))
+        
+        assert ds_len == len(ds)
+        
+            
+        return ds_len, ds
+    
     def __len__(self):
-        return len(self.fp)
+        return self.dataset_len
     
     def __getitem__(self, index):
-        '''
-           return a tensor of all sentences with shape num_sent * bert_embedding_size, a question tensor,
-           and a label temsor with size num_sent * 1
-        '''
-        qdict = self.fp[index]
-        supports = qdict['supporting_facts'] # a list of lists of facts with form [title, sent_id]
-        question = qdict['question']
-        context = qdict['context'] # a list of 2-element list [title, paragraph]
-        num_context = len(context)
-        
-        answer = qdict['answer'] # do not really need it?
-        qid = qdict['_id']
-        qtype = qdict['type']
-        
-        # some articles in the fullwiki dev/test sets have zero paragraphs
-        if len(context) == 0:
-            context = [['some random title', 'some random stuff']]
-        
-        num_sent = 0
-        sents = []
-        labels = []
-        for title, paragraph in context:
-            num_sent += len(paragraph)
-            sent_tensor = torch.as_tensor(self.bertClient.encode(paragraph)) # encode entire paragraph ,input: list of strs 
-            sents.append(sent_tensor) # size: n * 1024
-            for sent_index, sent in enumerate(paragraph):
-                sent_label = 1 if [title, sent_index] in supports else 0
-                
-                label_tensor = torch.as_tensor(sent_label)
-                labels.append(label_tensor.unsqueeze_(0))
-                
-        sent_mat = torch.cat(sents,0)
-        label_mat = torch.cat(labels,0)
-        
-        assert num_sent == sent_mat.size(0) == label_mat.size(0)
-        question_vec = torch.as_tensor(self.bertClient.encode([question]))
-        
-        return question_vec, sent_mat, label_mat
+        return self.dataset[index]
+#    def __getitem__(self, index):
+#        '''
+#           return a tensor of all sentences with shape num_sent * bert_embedding_size, a question tensor,
+#           and a label temsor with size num_sent * 1
+#        '''
+#        qdict = self.fp[index]
+#        supports = qdict['supporting_facts'] # a list of lists of facts with form [title, sent_id]
+#        question = qdict['question']
+#        context = qdict['context'] # a list of 2-element list [title, paragraph], where paragraph is a list of sentences.
+#        num_context = len(context)
+#        
+#        answer = qdict['answer'] # do not really need it?
+#        qid = qdict['_id']
+#        qtype = qdict['type']
+#        
+#        # some articles in the fullwiki dev/test sets have zero paragraphs
+#        if len(context) == 0:
+#            context = [['some random title', ['some random stuff']]]
+#        
+#        num_sent = 0
+#        sents = []
+#        labels = []
+#        for title, paragraph in context:
+#            num_sent += len(paragraph)
+#            sent_list = paragraph # encode entire paragraph ,input: list of strs 
+#            sents.append(sent_tensor) # size: n * 1024
+##            for sent_index, sent in enumerate(paragraph):
+##                sent_label = 1 if [title, sent_index] in supports else 0
+##                label_tensor = torch.as_tensor(sent_label)
+##                labels.append(label_tensor.unsqueeze_(0))
+#                
+##        sent_mat = torch.cat(sents,0)
+##        label_mat = torch.cat(labels,0)
+#        
+##        assert num_sent == sent_mat.size(0) == label_mat.size(0)
+#        question_vec = torch.as_tensor(self.bertClient.encode([question]))
+#        
+#        return question_vec, sent_mat, label_mat
 
 # test case            
 if __name__ == '__main__':
-    ds = sentDataset(mode='test')
-    #dl = data.DataLoader(ds, batch_size = 1, shuffle = False, num_workers = 0)
-    # num_workers != 0 ==> Error no responding ???
-    
-    # dev_ds: len==314793, num_positive = 10298, pos% = 3.27 %
-    # train_ds: len==3703344, num_positive = 215662, pos% = 5.82 %
-    # test true_pos acc on dev_dataset: acc == 8810/10298 == 85.6%
-
+    ds = ParaDataset(mode='train')
+    '''
+    ds[0] = 
+    ("Which magazine was started first Arthur's Magazine or First for Women?",
+ ['Radio City (Indian radio station)'
+  "Radio City is India's first private FM radio station and was started on 3 July 2001.",
+  ' It broadcasts on 91.1 (earlier 91.0 in most cities) megahertz from Mumbai (where it was started in 2004), Bengaluru (started first in 2001), Lucknow and New Delhi (since 2003).',
+  ' It plays Hindi, English and regional songs.',
+  ' It was launched in Hyderabad in March 2006, in Chennai on 7 July 2006 and in Visakhapatnam October 2007.',
+  ' Radio City recently forayed into New Media in May 2008 with the launch of a music portal - PlanetRadiocity.com that offers music related news, videos, songs, and other music-related features.',
+  ' The Radio station currently plays a mix of Hindi and Regional music.',
+  ' Abraham Thomas is the CEO of the company.'],
+ 0)
+    '''
+    dl = data.DataLoader(ds, batch_size = 4, shuffle = False, num_workers = 0)
+ 
     '''
     for i, (q, sample, label) in enumerate(dl,0):
         if i > 2:
@@ -168,5 +214,12 @@ if __name__ == '__main__':
     
      sent_mat.view(1,-1)
     '''
+    #dl = data.DataLoader(ds, batch_size = 1, shuffle = False, num_workers = 0, collate_fn=my_collate,)
+    # num_workers != 0 ==> Error no responding ???
+    
+    # dev_ds: len==314793, num_positive = 10298, pos% = 3.27 %
+    # train_ds: len==3703344, num_positive = 215662, pos% = 5.82 %
+    # test true_pos acc on dev_dataset: acc == 8810/10298 == 85.6%
+
         
         
