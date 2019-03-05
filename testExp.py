009b97906bf8463f33b1374e1c16a5285c4ab935
loveritsu929 <loveritsu929@gmail.com>
2019-03-05 17:19:37 -0500

    test

--- testExp.py
+++ testExp.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import time
 import os, copy
 import torch
 import torch.nn as nn
@@ -7,7 +6,6 @@ import torch.optim as optim
 import torch.utils.data as data
 import torch.cuda as cuda
 import numpy as np
-import sys
 
 import NNetworks
 import dlDataset
@@ -18,12 +16,12 @@ from bert_serving.client import BertClient
 device = torch.device('cuda:0')
 
 batchSize = 2048
-threshold = 0.1
+threshold = 0.001
 modelSaveDir = './exps/lstm_para_2'
 
 testDataset = dlDataset.ParaDataset(mode='test')
 #loss_weights = torch.as_tensor(sklearn.utils.class_weight.compute_class_weight('balanced', [0,1], testDataset.label_array[:10240]))
-testLoader = data.DataLoader(testDataset, batch_size = batchSize, shuffle = False, num_workers = 0)
+testLoader = data.DataLoader(testDataset, batch_size = batchSize, drop_last = True, shuffle = False, num_workers = 0)
 
 bert = BertClient(check_length=False)
 
@@ -38,7 +36,9 @@ def test_model(model, current_acc):
 
     running_corrects = 0
     true_positive = 0
+    false_positive = 0
     num_true = 0
+    num_pred_true = 0
     
     # ds: q, title+para , label
     for i, (q, para, label) in enumerate(testLoader, 0):
@@ -67,12 +67,16 @@ def test_model(model, current_acc):
         
         running_corrects += torch.sum(preds == label.data)
         true_positive += torch.sum(preds * label.data)
+        false_positive += np.sum(np.logical_and(preds.cpu().numpy(), (label==0).cpu().numpy()))
         num_true += torch.sum(label.data)
+        num_pred_true += torch.sum(preds)
         
         true_pos_acc = true_positive.double() / num_true.double()
+        false_pos_rate = false_positive / num_pred_true.double()
+        
         
         if i % 10 == 0:
-            print('Testing Iteration {}: true_pos = {} true_pos_acc = {}'.format(i, true_positive, true_pos_acc))
+            print('Iter {}: true_pos = {} TP_acc = {:.4f} FP% = {:.4f}'.format(i, true_positive, true_pos_acc, false_pos_rate))
         
     
     acc = running_corrects.double() / len(testDataset)  
